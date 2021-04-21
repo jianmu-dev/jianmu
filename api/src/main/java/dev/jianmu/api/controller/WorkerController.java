791155f05d1bc6ae32e652ce0c3ab69aae43b6b5
ethan-liu <ethan-liu@outlook.com>
2021-04-21 14:43:39 +0800

    Add Worker Type API

---
+++ api/src/main/java/dev/jianmu/api/controller/WorkerController.java
@@ -0,0 +1,25 @@
+package dev.jianmu.api.controller;
+
+import dev.jianmu.task.aggregate.Worker;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @class: WorkerController
+ * @description: Worker API
+ * @author: Ethan Liu
+ * @create: 2021-04-21 14:40
+ **/
+@RestController
+@RequestMapping("workers")
+@Tag(name = "Worker API", description = "Worker API")
+public class WorkerController {
+    @GetMapping("/types")
+    @Operation(summary = "Worker类型获取接口", description = "Worker类型获取接口")
+    public Worker.Type[] getTypes() {
+        return Worker.Type.values();
+    }
+}
