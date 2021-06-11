20a6ddc1145212cf724c3ea5caf895e7e17930d9
ethan-liu <ethan-liu@outlook.com>
2021-06-11 16:22:20 +0800

    refactor: 删除无用接口

--- api/src/main/java/dev/jianmu/api/controller/LogController.java
+++ api/src/main/java/dev/jianmu/api/controller/LogController.java
@@ -2,7 +2,6 @@ package dev.jianmu.api.controller;
 
 import dev.jianmu.infrastructure.storage.StorageService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
@@ -14,13 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import javax.inject.Inject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @class: LogController
@@ -39,22 +33,6 @@ public class LogController {
     @Inject
     public LogController(StorageService storageService) {
         this.storageService = storageService;
-    }
-
-    @GetMapping("/{logId}")
-    @Operation(summary = "日志获取接口", description = "日志获取接口，返回日志流")
-    public StreamingResponseBody logStream(@Parameter(description = "任务实例ID即为日志ID") @PathVariable String logId) {
-        BufferedReader reader = storageService.readLog(logId);
-        return (os -> {
-            readAndWrite(reader, os);
-        });
-    }
-
-    private void readAndWrite(final BufferedReader reader, OutputStream os) throws IOException {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            os.write((line + "\n").getBytes(StandardCharsets.UTF_8));
-        }
     }
 
     @GetMapping("/new/{logId}")
