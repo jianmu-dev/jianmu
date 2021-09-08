69b65e06ade0d34a64b44216be5e8a429a0b78da
ethan-liu <ethan-liu@outlook.com>
2021-09-08 16:31:26 +0800

    refactor: 任务创建代码重构

--- application/src/main/java/dev/jianmu/application/service/TaskInstanceApplication.java
+++ application/src/main/java/dev/jianmu/application/service/TaskInstanceApplication.java
@@ -4,25 +4,30 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.jianmu.application.exception.DataNotFoundException;
+import dev.jianmu.application.query.NodeDefApi;
+import dev.jianmu.el.ElContext;
 import dev.jianmu.eventbridge.repository.TargetEventRepository;
-import dev.jianmu.parameter.aggregate.Parameter;
-import dev.jianmu.parameter.repository.ParameterRepository;
+import dev.jianmu.hub.intergration.aggregate.NodeParameter;
 import dev.jianmu.task.aggregate.Definition;
 import dev.jianmu.task.aggregate.InstanceParameter;
 import dev.jianmu.task.aggregate.TaskInstance;
-import dev.jianmu.task.repository.*;
+import dev.jianmu.task.repository.DefinitionRepository;
+import dev.jianmu.task.repository.InstanceParameterRepository;
+import dev.jianmu.task.repository.TaskInstanceRepository;
 import dev.jianmu.task.service.InstanceDomainService;
-import dev.jianmu.task.service.InstanceParameterDomainService;
+import dev.jianmu.workflow.aggregate.parameter.Parameter;
+import dev.jianmu.workflow.el.ExpressionLanguage;
+import dev.jianmu.workflow.event.TaskActivatingEvent;
+import dev.jianmu.workflow.repository.ParameterRepository;
+import dev.jianmu.workflow.repository.WorkflowRepository;
+import dev.jianmu.workflow.service.ParameterDomainService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -36,33 +41,39 @@ public class TaskInstanceApplication {
     private static final Logger logger = LoggerFactory.getLogger(TaskInstanceApplication.class);
 
     private final TaskInstanceRepository taskInstanceRepository;
+    private final WorkflowRepository workflowRepository;
     private final DefinitionRepository definitionRepository;
     private final InstanceDomainService instanceDomainService;
     private final ParameterRepository parameterRepository;
-    private final ParameterReferRepository parameterReferRepository;
+    private final ParameterDomainService parameterDomainService;
     private final TargetEventRepository targetEventRepository;
     private final InstanceParameterRepository instanceParameterRepository;
-    private final InputParameterRepository inputParameterRepository;
+    private final NodeDefApi nodeDefApi;
+    private final ExpressionLanguage expressionLanguage;
 
     @Inject
     public TaskInstanceApplication(
             TaskInstanceRepository taskInstanceRepository,
+            WorkflowRepository workflowRepository,
             DefinitionRepository definitionRepository,
             InstanceDomainService instanceDomainService,
             ParameterRepository parameterRepository,
-            ParameterReferRepository parameterReferRepository,
+            ParameterDomainService parameterDomainService,
             TargetEventRepository targetEventRepository,
             InstanceParameterRepository instanceParameterRepository,
-            InputParameterRepository inputParameterRepository
+            NodeDefApi nodeDefApi,
+            ExpressionLanguage expressionLanguage
     ) {
         this.taskInstanceRepository = taskInstanceRepository;
+        this.workflowRepository = workflowRepository;
         this.definitionRepository = definitionRepository;
         this.instanceDomainService = instanceDomainService;
         this.parameterRepository = parameterRepository;
-        this.parameterReferRepository = parameterReferRepository;
+        this.parameterDomainService = parameterDomainService;
         this.targetEventRepository = targetEventRepository;
         this.instanceParameterRepository = instanceParameterRepository;
-        this.inputParameterRepository = inputParameterRepository;
+        this.nodeDefApi = nodeDefApi;
+        this.expressionLanguage = expressionLanguage;
     }
 
     public List<InstanceParameter> findParameters(String instanceId) {
@@ -78,58 +89,99 @@ public class TaskInstanceApplication {
     }
 
     @Transactional
-    public void create(
-            String businessId,
-            String workflowRef,
-            String workflowVersion,
-            String triggerId,
-            String asyncTaskRef,
-            String asyncTaskType
-    ) {
+    public void create(TaskActivatingEvent event) {
+        var workflow = this.workflowRepository.findByRefAndVersion(event.getWorkflowRef(), event.getWorkflowVersion())
+                .orElseThrow(() -> new DataNotFoundException("未找到流程定义: " + event.getWorkflowRef()));
+        var asyncTask = workflow.findNode(event.getNodeRef());
+        var nodeDef = this.nodeDefApi.findByType(asyncTask.getType())
+                .orElseThrow(() -> new DataNotFoundException("未找到节点定义：" + asyncTask.getType()));
         // 创建任务实例
-        String[] strings = asyncTaskType.split(":");
-        Definition definition = this.definitionRepository.findByRefAndVersion(strings[0], strings[1])
-                .orElseThrow(() -> new DataNotFoundException("未找到任务定义"));
-        List<TaskInstance> taskInstances = this.taskInstanceRepository.findByAsyncTaskRefAndBusinessId(asyncTaskRef, businessId);
-        TaskInstance taskInstance = this.instanceDomainService.create(taskInstances, definition, businessId, triggerId, asyncTaskRef, workflowRef, workflowVersion);
+        List<TaskInstance> taskInstances = this.taskInstanceRepository.findByAsyncTaskRefAndBusinessId(event.getNodeRef(), event.getWorkflowInstanceId());
+        // 运行前检查规则
+        this.instanceDomainService.runningCheck(taskInstances);
+        var taskInstance = TaskInstance.Builder.anInstance()
+                .serialNo(taskInstances.size() + 1)
+                .defKey(asyncTask.getType())
+                .asyncTaskRef(asyncTask.getRef())
+                .workflowRef(workflow.getRef())
+                .workflowVersion(workflow.getVersion())
+                .businessId(event.getWorkflowInstanceId())
+                .triggerId(event.getTriggerId())
+                .build();
+        // 查询参数源
+        var eventParameters = this.targetEventRepository.findById(event.getTriggerId())
+                .orElseThrow(() -> new DataNotFoundException("未找到事件" + event.getTriggerId()))
+                .getEventParameters();
+        var instanceParameters = this.instanceParameterRepository
+                .findOutputParamByBusinessIdAndTriggerId(event.getWorkflowInstanceId(), event.getTriggerId());
+        // 创建表达式上下文
+        var context = new ElContext();
+        // 全局参数加入上下文
+        workflow.getGlobalParameters()
+                .forEach(globalParameter -> context.add("global", globalParameter.getName(), Parameter.Type.STRING.newParameter(globalParameter.getValue())));
+        // 事件参数加入上下文
+        var eventParams = eventParameters.stream()
+                .map(eventParameter -> Map.entry(eventParameter.getName(), eventParameter.getParameterId()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        var eventParamValues = this.parameterRepository.findByIds(new HashSet<>(eventParams.values()));
+        var eventMap = this.parameterDomainService.matchParameters(eventParams, eventParamValues);
+        // 事件参数scope为event
+        eventMap.forEach((key, val) -> context.add("event", key, val));
+        // 任务输出参数加入上下文
+        var outParams = instanceParameters.stream()
+                // 输出参数scope为asyncTaskRef
+                .map(instanceParameter -> Map.entry(instanceParameter.getAsyncTaskRef() + "." + instanceParameter.getRef(), instanceParameter.getParameterId()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        var outParamValues = this.parameterRepository.findByIds(new HashSet<>(outParams.values()));
+        var outMap = this.parameterDomainService.matchParameters(outParams, outParamValues);
+        outMap.forEach(context::add);
 
-        // 查询流程定义参数关联
-        var refers = this.parameterReferRepository
-                .findByRefAndVersionAndTargetTaskRef(workflowRef, workflowVersion, taskInstance.getAsyncTaskRef());
-        // 查询关联事件参数与任务输出参数
-        var instanceOutputParameters = refers.stream().map(refer -> {
-            // 查询关联的事件参数
-            if (refer.getSourceTaskRef().equals("Event")) {
-                var targetEvent = this.targetEventRepository.findById(triggerId)
-                        .orElseThrow(() -> new DataNotFoundException("未找到事件" + triggerId));
-                var eventParameter = targetEvent.getEventParameter(refer.getSourceParameterRef())
-                        .orElseThrow(() -> new DataNotFoundException("未找到该事件参数" + refer.getSourceParameterRef()));
-                var instanceParameter = InstanceParameter.Builder.anInstanceParameter()
-                        .parameterId(eventParameter.getParameterId())
-                        .build();
-                return Map.entry(refer.getTargetParameterRef(), instanceParameter);
-            } else {
-                // 查询关联的任务输出参数
-                var instanceParameter = this.instanceParameterRepository
-                        .findInputParamByBusinessIdAndTaskRefAndRefAndMaxSerial(businessId, refer.getSourceTaskRef(), refer.getSourceParameterRef())
-                        .orElseThrow(() -> new DataNotFoundException("未找到关联的任务输出参数"));
-                return Map.entry(refer.getTargetParameterRef(), instanceParameter);
-            }
-        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-        // 查询输入参数
-        var inputParameters = this.inputParameterRepository
-                .findByWorkflowRefAndWorkflowVersionAndAsyncTaskRef(workflowRef, workflowVersion, asyncTaskRef);
-        // 任务输入参数与关联输出参数的参数值覆盖
-        var taskInputParameters = definition.getInputParametersWith(inputParameters, instanceOutputParameters);
+        workflow.setExpressionLanguage(this.expressionLanguage);
+        workflow.setContext(context);
+        var params = workflow.calculateTaskParams(asyncTask.getRef());
 
         // 创建任务实例输入参数
-        var instanceInputParameters = InstanceParameterDomainService
-                .createInstanceParameters(taskInputParameters, taskInstance);
+        var instanceInputParameters = this.createInstanceParameters(params, taskInstance, nodeDef.getInputParameters());
 
+        // 保存参数
+        this.parameterRepository.addAll(new ArrayList<>(params.values()));
         // 保存任务实例输入参数
         this.instanceParameterRepository.addAll(instanceInputParameters);
         // 保存任务实例
         this.taskInstanceRepository.add(taskInstance);
+    }
+
+    private Set<InstanceParameter> createInstanceParameters(Map<String, Parameter<?>> parameterMap, TaskInstance taskInstance, Set<NodeParameter> nodeParameters) {
+        var instanceParameters = parameterMap.entrySet().stream().map(entry ->
+                InstanceParameter.Builder.anInstanceParameter()
+                        .instanceId(taskInstance.getId())
+                        .triggerId(taskInstance.getTriggerId())
+                        .defKey(taskInstance.getDefKey())
+                        .asyncTaskRef(taskInstance.getAsyncTaskRef())
+                        .businessId(taskInstance.getBusinessId())
+                        .ref(entry.getKey())
+                        .parameterId(entry.getValue().getId())
+                        .type(InstanceParameter.Type.INPUT)
+                        .build()
+        ).collect(Collectors.toSet());
+        // 合并节点定义的默认参数与DSL中指定的参数
+        nodeParameters.forEach(nodeParameter -> {
+            // 如果不存在则使用默认参数
+            if (!parameterMap.containsKey(nodeParameter.getRef())) {
+                var p = InstanceParameter.Builder.anInstanceParameter()
+                        .instanceId(taskInstance.getId())
+                        .triggerId(taskInstance.getTriggerId())
+                        .defKey(taskInstance.getDefKey())
+                        .asyncTaskRef(taskInstance.getAsyncTaskRef())
+                        .businessId(taskInstance.getBusinessId())
+                        .ref(nodeParameter.getRef())
+                        .parameterId(nodeParameter.getParameterId())
+                        .type(InstanceParameter.Type.INPUT)
+                        .build();
+                instanceParameters.add(p);
+            }
+        });
+        return instanceParameters;
     }
 
     @Transactional
