a3712ad30b8418afdc374d62e6179101e1ddc5d7
ethan-liu <ethan-liu@outlook.com>
2021-09-09 11:48:44 +0800

    refactor: 适配Hub新接口

--- infrastructure/src/main/java/dev/jianmu/infrastructure/client/RegistryClient.java
+++ infrastructure/src/main/java/dev/jianmu/infrastructure/client/RegistryClient.java
@@ -1,9 +1,5 @@
 package dev.jianmu.infrastructure.client;
 
-import dev.jianmu.task.aggregate.Definition;
-import dev.jianmu.task.aggregate.DockerDefinition;
-import dev.jianmu.task.aggregate.MetaData;
-import dev.jianmu.task.aggregate.Worker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -28,39 +24,29 @@ public class RegistryClient {
         this.registryProperties = registryProperties;
     }
 
-    public Optional<? extends Definition> findByRefAndVersion(String ref, String version) {
-        DockerDefinition definition = null;
+    public Optional<NodeDefinitionDto> findByRef(String ref) {
         try {
-            var downloadUrl = registryProperties.getUrl() + "/view/definitions/" + ref + "/" + version;
-            log.info("从 {} 下载节点定义: {}:{} ", downloadUrl, ref, version);
-            var dto = this.restTemplate.getForObject(downloadUrl, DefinitionDto.class);
-            log.info("已下载节点定义: {}:{} 内容为: {}", ref, version, dto);
-            var type = Worker.Type.valueOf(dto.getType());
-            var metaData = MetaData.Builder.aMetaData()
-                    .name(dto.getMetaData().getName())
-                    .description(dto.getMetaData().getDescription())
-                    .icon(dto.getMetaData().getIcon())
-                    .group(dto.getMetaData().getGroup())
-                    .tags(dto.getMetaData().getTags())
-                    .docs(dto.getMetaData().getDocs())
-                    .owner(dto.getMetaData().getOwner())
-                    .source(dto.getMetaData().getSource())
-                    .build();
-            if (type.equals(Worker.Type.DOCKER)) {
-                definition = DockerDefinition.Builder.aDockerDefinition()
-                        .ref(dto.getRef())
-                        .version(dto.getVersion())
-                        .resultFile(dto.getResultFile())
-                        .type(type)
-                        .inputParameters(dto.getInputParameters())
-                        .outputParameters(dto.getOutputParameters())
-                        .metaData(metaData)
-                        .spec(dto.getSpec())
-                        .build();
-            }
+            var downloadUrl = registryProperties.getUrl() + "/download/node_definitions/" + ref;
+            log.info("从 {} 下载节点定义", downloadUrl);
+            var dto = this.restTemplate.getForObject(downloadUrl, NodeDefinitionDto.class);
+            log.info("已下载节点定义: {} 内容为: {}", ref, dto);
+            return Optional.ofNullable(dto);
         } catch (HttpClientErrorException | NullPointerException e) {
             log.info("未找到节点定义或节点定义内容不正确: {}", e.getMessage());
         }
-        return Optional.ofNullable(definition);
+        return Optional.empty();
+    }
+
+    public Optional<NodeDefinitionVersionDto> findByRefAndVersion(String ref, String version) {
+        try {
+            var downloadUrl = registryProperties.getUrl() + "/download/node_definitions/" + ref + "/versions/" + version;
+            log.info("从 {} 下载节点定义版本", downloadUrl);
+            var dto = this.restTemplate.getForObject(downloadUrl, NodeDefinitionVersionDto.class);
+            log.info("已下载节点定义版本: {}:{} 内容为: {}", ref, version, dto);
+            return Optional.ofNullable(dto);
+        } catch (HttpClientErrorException | NullPointerException e) {
+            log.info("未找到节点定义版本或节点定义版本内容不正确: {}", e.getMessage());
+        }
+        return Optional.empty();
     }
 }
