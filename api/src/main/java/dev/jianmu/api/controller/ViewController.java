9a41bfffbd4714acd5ca1e2437ab4daba24df968
ethan-liu <ethan-liu@outlook.com>
2021-09-27 21:07:03 +0800

    perf: 优化接口

--- api/src/main/java/dev/jianmu/api/controller/ViewController.java
+++ api/src/main/java/dev/jianmu/api/controller/ViewController.java
@@ -1,15 +1,18 @@
 package dev.jianmu.api.controller;
 
+import com.github.pagehelper.PageInfo;
+import dev.jianmu.api.dto.NamespaceSearchDto;
+import dev.jianmu.api.dto.PageDto;
 import dev.jianmu.api.mapper.ProjectMapper;
 import dev.jianmu.api.mapper.TaskInstanceMapper;
 import dev.jianmu.api.mapper.WorkflowInstanceMapper;
 import dev.jianmu.api.vo.*;
 import dev.jianmu.application.exception.DataNotFoundException;
-import dev.jianmu.application.service.ParameterApplication;
-import dev.jianmu.application.service.ProjectApplication;
-import dev.jianmu.application.service.TaskInstanceApplication;
-import dev.jianmu.application.service.WorkflowInstanceApplication;
+import dev.jianmu.application.service.*;
+import dev.jianmu.hub.intergration.aggregate.NodeDefinitionVersion;
 import dev.jianmu.infrastructure.storage.StorageService;
+import dev.jianmu.secret.aggregate.KVPair;
+import dev.jianmu.secret.aggregate.Namespace;
 import dev.jianmu.task.aggregate.InstanceParameter;
 import dev.jianmu.workflow.aggregate.process.AsyncTaskInstance;
 import dev.jianmu.workflow.aggregate.process.ProcessStatus;
@@ -40,6 +43,8 @@ import java.util.stream.Collectors;
 @Tag(name = "查询API", description = "查询API")
 public class ViewController {
     private final ProjectApplication projectApplication;
+    private final HubApplication hubApplication;
+    private final SecretApplication secretApplication;
     private final WorkflowInstanceApplication instanceApplication;
     private final TaskInstanceApplication taskInstanceApplication;
     private final ParameterApplication parameterApplication;
@@ -47,16 +52,65 @@ public class ViewController {
 
     public ViewController(
             ProjectApplication projectApplication,
+            HubApplication hubApplication,
+            SecretApplication secretApplication,
             WorkflowInstanceApplication instanceApplication,
             TaskInstanceApplication taskInstanceApplication,
             ParameterApplication parameterApplication,
             StorageService storageService
     ) {
         this.projectApplication = projectApplication;
+        this.hubApplication = hubApplication;
+        this.secretApplication = secretApplication;
         this.instanceApplication = instanceApplication;
         this.taskInstanceApplication = taskInstanceApplication;
         this.parameterApplication = parameterApplication;
         this.storageService = storageService;
+    }
+
+    @GetMapping("/namespaces")
+    @Operation(summary = "分页查询命名空间列表", description = "分页查询命名空间列表")
+    public PageInfo<Namespace> findAll(NamespaceSearchDto namespaceSearchDto) {
+        return this.secretApplication.findAll(namespaceSearchDto.getName(), namespaceSearchDto.getPageNum(), namespaceSearchDto.getPageSize());
+    }
+
+    @GetMapping("/namespaces/{name}/keys")
+    @Operation(summary = "查询键值对列表", description = "查询键值对列表")
+    public List<String> findAll(@PathVariable String name) {
+        var kvs = this.secretApplication.findAll(name);
+        return kvs.stream().map(KVPair::getKey).collect(Collectors.toList());
+    }
+
+    @GetMapping("/nodes")
+    @Operation(summary = "节点定义列表", description = "节点定义列表查询")
+    public PageInfo<NodeDefVo> findNodeAll(PageDto dto) {
+        var page = this.hubApplication.findPage(
+                dto.getPageNum(),
+                dto.getPageSize()
+        );
+        var nodes = page.getList();
+        List<NodeDefVo> nodeDefVos = nodes.stream().map(nodeDefinition -> {
+            var versions = this.hubApplication.findByOwnerRefAndRef(nodeDefinition.getOwnerRef(), nodeDefinition.getRef()).stream()
+                    .map(NodeDefinitionVersion::getVersion).collect(Collectors.toList());
+            return NodeDefVo.builder()
+                    .icon(nodeDefinition.getIcon())
+                    .name(nodeDefinition.getName())
+                    .ownerName(nodeDefinition.getOwnerName())
+                    .ownerType(nodeDefinition.getOwnerType())
+                    .ownerRef(nodeDefinition.getOwnerRef())
+                    .creatorName(nodeDefinition.getCreatorName())
+                    .creatorRef(nodeDefinition.getCreatorRef())
+                    .type(nodeDefinition.getType())
+                    .description(nodeDefinition.getDescription())
+                    .ref(nodeDefinition.getRef())
+                    .sourceLink(nodeDefinition.getSourceLink())
+                    .documentLink(nodeDefinition.getDocumentLink())
+                    .versions(versions)
+                    .build();
+        }).collect(Collectors.toList());
+        PageInfo<NodeDefVo> newPage = PageUtils.pageInfo2PageInfoVo(page);
+        newPage.setList(nodeDefVos);
+        return newPage;
     }
 
     @GetMapping("/projects")
