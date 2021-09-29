8a0bd00d05fab13bc36d8b8acd1d033577c2affa
ethan-liu <ethan-liu@outlook.com>
2021-09-29 14:08:24 +0800

    feat: 添加对Hub的AK/SK支持

--- infrastructure/src/main/java/dev/jianmu/infrastructure/client/RegistryClient.java
+++ infrastructure/src/main/java/dev/jianmu/infrastructure/client/RegistryClient.java
@@ -1,10 +1,16 @@
 package dev.jianmu.infrastructure.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 /**
@@ -24,11 +30,36 @@ public class RegistryClient {
         this.registryProperties = registryProperties;
     }
 
+    private HttpHeaders createHeaders(String path) {
+        var headers = new HttpHeaders();
+        if (registryProperties.getAk() == null ||
+                registryProperties.getSk() == null ||
+                registryProperties.getAk().isBlank() ||
+                registryProperties.getSk().isBlank()
+        ) {
+            return headers;
+        }
+        try {
+            var signature = HmacSha1.encrypt(path, registryProperties.getSk());
+            headers.add("X-Access-Key", registryProperties.getAk());
+            headers.add("X-Signature", signature);
+            return headers;
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("签名失败");
+        }
+    }
+
+    private <T> T request(String path, Class<T> tClass) {
+        var downloadUrl = registryProperties.getUrl() + path;
+        var headers = this.createHeaders(path);
+        var entity = new HttpEntity<>(headers);
+        return this.restTemplate.exchange(downloadUrl, HttpMethod.GET, entity, tClass).getBody();
+    }
+
     public Optional<NodeDefinitionDto> findByRef(String ref) {
         try {
-            var downloadUrl = registryProperties.getUrl() + "/hub/download/node_definitions/" + ref;
-            log.info("从 {} 下载节点定义", downloadUrl);
-            var dto = this.restTemplate.getForObject(downloadUrl, NodeDefinitionDto.class);
+            var path = "/hub/download/node_definitions/" + ref;
+            var dto = this.request(path, NodeDefinitionDto.class);
             log.info("已下载节点定义: {} 内容为: {}", ref, dto);
             return Optional.ofNullable(dto);
         } catch (HttpClientErrorException | NullPointerException e) {
@@ -39,9 +70,8 @@ public class RegistryClient {
 
     public Optional<NodeDefinitionVersionDto> findByRefAndVersion(String ref, String version) {
         try {
-            var downloadUrl = registryProperties.getUrl() + "/hub/download/node_definitions/" + ref + "/versions/" + version;
-            log.info("从 {} 下载节点定义版本", downloadUrl);
-            var dto = this.restTemplate.getForObject(downloadUrl, NodeDefinitionVersionDto.class);
+            var path = "/hub/download/node_definitions/" + ref + "/versions/" + version;
+            var dto = this.request(path, NodeDefinitionVersionDto.class);
             log.info("已下载节点定义版本: {}:{} 内容为: {}", ref, version, dto);
             return Optional.ofNullable(dto);
         } catch (HttpClientErrorException | NullPointerException e) {
