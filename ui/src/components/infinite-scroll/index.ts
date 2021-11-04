aae30b944691333b9ad7953e6e1d96cfc490211c
comyan <1628126452@qq.com>
2021-11-04 11:23:15 +0800

    feat: infinite scorll directive

--- ui/src/components/infinite-scroll/index.ts
+++ ui/src/components/infinite-scroll/index.ts
@@ -1,5 +1,5 @@
-import type { App, DirectiveBinding, ObjectDirective } from 'vue';
-// 查找指令挂载的根节点
+import type { App, DirectiveBinding } from 'vue';
+import { CustomObjectDirective } from './model';
 const findRootElement = (el: HTMLElement | null): HTMLElement | null => {
   // 递归发现没父元素直接结束
   if (!el) {
@@ -15,31 +15,31 @@ function getStyle(element: HTMLElement, attr: string) {
   if (element.currentStyle) {
     // @ts-ignore
     return element.currentStyle[attr];
-  } else {
-    // @ts-ignore
-    return getComputedStyle(element, null)[attr];
   }
+  // @ts-ignore
+  return getComputedStyle(element, null)[attr];
 }
 // 指令
 const vScroll = {
-  // 挂载指令
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     let rootElement = findRootElement(el);
+    const dir = binding.dir as CustomObjectDirective;
     // 默认设置挂载元素高度
     if (binding.modifiers.current) {
       rootElement = el;
-      // 为找到挂载指令的元素
-      if (!rootElement) {
-        return;
-      }
       // rootElement指定为当前节点，设置height直接为浏览器computed自动计算的值
-      const height = getStyle(el, 'height');
+      const height = getStyle(rootElement, 'height');
       // 浏览器计算出来的高度为0，提示警告
       if (!height) {
         console.warn('无法拿到元素初始高度，请设置元素高度');
         return;
       }
-      rootElement.style.height = getStyle(el, 'height');
+      if (height.includes('%')) {
+        console.warn('请设置具体的元素的高度');
+        return;
+      }
+      rootElement.style.height = height;
+      dir.initHeight = rootElement.clientHeight;
     } else {
       if (!rootElement) {
         return;
@@ -47,7 +47,6 @@ const vScroll = {
       rootElement.style.height = '100vh';
     }
     rootElement.style.overflowY = 'auto';
-    const dir = binding.dir as ObjectDirective & { handler: () => void };
     dir.handler = function () {
       if (
         !rootElement ||
@@ -61,8 +60,23 @@ const vScroll = {
     };
     rootElement.addEventListener('scroll', dir.handler);
   },
-  // 卸载指令
-  unmounted(el: HTMLElement, binding: DirectiveBinding) {
+  updated(el: HTMLElement, binding: DirectiveBinding) {
+    let rootElement = findRootElement(el);
+    if (binding.modifiers.current) {
+      rootElement = el;
+      const dir = binding.dir as CustomObjectDirective;
+      if (dir.timer) {
+        clearTimeout(dir.timer);
+      }
+      dir.timer = setTimeout(() => {
+        if (rootElement!.scrollHeight > (dir.initHeight as number)) {
+          return;
+        }
+        binding.value();
+      }, 400);
+    }
+  },
+  beforeUnmount(el: HTMLElement, binding: DirectiveBinding) {
     let rootElement = findRootElement(el);
     if (binding.modifiers.current) {
       rootElement = el;
@@ -70,9 +84,10 @@ const vScroll = {
     if (!rootElement) {
       return;
     }
-    const { handler } = binding.dir as ObjectDirective & {
-      handler: () => void;
-    };
+    const { handler, timer } = binding.dir as CustomObjectDirective;
+    if (timer) {
+      clearTimeout(timer);
+    }
     rootElement.removeEventListener('scroll', handler);
   },
 };
