7bbaa24b308a31b4979ac1b9421f2942ab146e0e
ethan-liu <ethan-liu@outlook.com>
2021-11-04 11:49:23 +0800

    refactor: 调整集成Vault的配置项

--- infrastructure/src/main/java/dev/jianmu/infrastructure/credential/CredentialProperties.java
+++ infrastructure/src/main/java/dev/jianmu/infrastructure/credential/CredentialProperties.java
@@ -2,7 +2,13 @@ package dev.jianmu.infrastructure.credential;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * @class: VaultProperties
@@ -12,10 +18,43 @@ import org.springframework.stereotype.Component;
  **/
 @Data
 @Component
+@Validated
 @ConfigurationProperties(prefix = "credential")
 public class CredentialProperties {
+    @NotBlank
     private String type;
-    private String url;
-    private String token;
-    private String vaultEngineName;
+    private VaultProperties vault;
+
+    @Data
+    @Component
+    @Validated
+    public static class VaultProperties {
+        public enum AuthenticationType {
+            TOKEN,
+            CERT
+        }
+
+        @NotNull
+        private String url;
+        private String vaultEngineName;
+        private AuthenticationType authentication;
+        private String token;
+        private SslProperties ssl;
+        private int connectionTimeout = 5000;
+        private int readTimeout = 15000;
+
+        @Data
+        @Component
+        @Validated
+        public static class SslProperties {
+            private Resource keyStore;
+            private String keyStorePassword;
+            private Resource trustStore;
+            private String trustStorePassword;
+            private String keyStoreType;
+            private String trustStoreType;
+            @NotEmpty
+            private String certAuthPath = "cert";
+        }
+    }
 }
