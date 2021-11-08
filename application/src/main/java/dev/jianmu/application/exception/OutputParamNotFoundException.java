2b40beaff48500b2c38951b95ba7d304dce31b83
daihw <928874202@qq.com>
2021-11-08 15:55:16 +0800

    feat: 添加必填输出参数的校验

---
+++ application/src/main/java/dev/jianmu/application/exception/OutputParamNotFoundException.java
@@ -0,0 +1,13 @@
+package dev.jianmu.application.exception;
+
+/**
+ * @class: DslException
+ * @description: DslException
+ * @author: daihw
+ * @create: 2021/11/8 3:46 下午
+ */
+public class OutputParamNotFoundException extends RuntimeException {
+    public OutputParamNotFoundException(String message) {
+        super(message);
+    }
+}
