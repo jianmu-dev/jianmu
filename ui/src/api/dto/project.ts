4f88b6901a564ba59e6fb2e88d9eddf18e3315ab
liamjung <liamjung@live.com>
2021-11-11 12:37:18 +0800

    refactor: 优化代码

--- ui/src/api/dto/project.ts
+++ ui/src/api/dto/project.ts
@@ -70,7 +70,7 @@ export interface IProjectVo extends Readonly<BaseVo & {
   source: DslSourceEnum;
   dslType: DslTypeEnum;
   gitRepoId?: string;
-  startTime: string;
+  startTime?: string;
   latestTime?: string;
   nextTime?: string;
   status: ProjectStatusEnum;
