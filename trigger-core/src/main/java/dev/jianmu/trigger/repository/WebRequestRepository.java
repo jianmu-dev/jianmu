91d796c03089f509b47a96308648118246613a42
ethan-liu <ethan-liu@outlook.com>
2021-11-23 11:16:39 +0800

    refactor: 添加Webhook重试接口

--- trigger-core/src/main/java/dev/jianmu/trigger/repository/WebRequestRepository.java
+++ trigger-core/src/main/java/dev/jianmu/trigger/repository/WebRequestRepository.java
@@ -2,12 +2,16 @@ package dev.jianmu.trigger.repository;
 
 import dev.jianmu.trigger.aggregate.WebRequest;
 
+import java.util.Optional;
+
 /**
+ * @author Ethan Liu
  * @class WebRequestRepository
  * @description WebRequestRepository
- * @author Ethan Liu
  * @create 2021-11-14 22:19
  */
 public interface WebRequestRepository {
     void add(WebRequest webRequest);
+
+    Optional<WebRequest> findById(String id);
 }
