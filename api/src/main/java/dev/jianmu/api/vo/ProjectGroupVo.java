ba8cb24a91b36180ab3e7aa9d0b4ddc904491b1d
daihw <928874202@qq.com>
2021-11-29 16:10:51 +0800

    fix: 修改项目组Vo的Schema

--- api/src/main/java/dev/jianmu/api/vo/ProjectGroupVo.java
+++ api/src/main/java/dev/jianmu/api/vo/ProjectGroupVo.java
@@ -21,19 +21,27 @@ import java.time.LocalDateTime;
 @Schema(description = "项目组Vo")
 public class ProjectGroupVo {
     //ID
+    @Schema(description = "ID")
     private String id;
     // 名称
+    @Schema(description = "名称")
     private String name;
     // 描述
+    @Schema(description = "描述")
     private String description;
     // 排序
+    @Schema(description = "排序")
     private Integer sort;
     // 项目数
+    @Schema(description = "项目数")
     private Integer projectCount;
     // 创建时间
+    @Schema(description = "创建时间")
     private LocalDateTime createdTime;
     // 最后修改时间
+    @Schema(description = "最后修改时间")
     private LocalDateTime lastModifiedTime;
     // 是否是默认分组
+    @Schema(description = "是否是默认分组")
     private Boolean isDefaultGroup;
 }
