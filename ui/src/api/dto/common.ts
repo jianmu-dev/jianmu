78f7d211fe16632f68d40d03c848cbc0b77a0e24
qbhfh <2682147541@qq.com>
2021-12-07 11:06:02 +0800

    fix: 还原了被自动保存改变格式的文件

--- ui/src/api/dto/common.ts
+++ ui/src/api/dto/common.ts
@@ -1,77 +1,77 @@
 /**
  * 错误信息
  */
-export interface IErrorMessageVo
-  extends Readonly<{
-    statusCode: number;
-    timestamp: Date;
-    message: string;
-    description: string;
-  }> {}
+export interface IErrorMessageVo extends Readonly<{
+  statusCode: number;
+  timestamp: Date;
+  message: string;
+  description: string;
+}> {
+}
 
 /**
  * 抽象值对象
  */
-export interface BaseVo
-  extends Readonly<{
-    /**
-     * 创建时间
-     */
-    createdTime: string;
+export interface BaseVo extends Readonly<{
+  /**
+   * 创建时间
+   */
+  createdTime: string;
 
-    /**
-     * 最后修改人
-     */
-    lastModifiedBy: string;
+  /**
+   * 最后修改人
+   */
+  lastModifiedBy: string;
 
-    /**
-     * 最后修改时间
-     */
-    lastModifiedTime: string;
-  }> {}
+  /**
+   * 最后修改时间
+   */
+  lastModifiedTime: string;
+}> {
+}
 
 /**
  * 分页dto
  */
-export interface IPageDto
-  extends Readonly<{
-    /**
-     * 页码
-     */
-    pageNum: number;
+export interface IPageDto extends Readonly<{
+  /**
+   * 页码
+   */
+  pageNum: number;
 
-    /**
-     * 每页个数
-     */
-    pageSize: number;
-  }> {}
+  /**
+   * 每页个数
+   */
+  pageSize: number;
+}> {
+}
 
 /**
  * 分页值对象
  */
-export interface IPageVo<T>
-  extends Readonly<{
-    /**
-     * 总个数
-     */
-    total: number;
+export interface IPageVo<T> extends Readonly<{
+  /**
+   * 总个数
+   */
+  total: number;
 
-    /**
-     * 总页数
-     */
-    pages: number;
+  /**
+   * 总页数
+   */
+  pages: number;
 
-    /**
-     * 数据
-     */
-    list: T[];
-  }> {}
+  /**
+   * 数据
+   */
+  list: T[];
+}> {
+}
 
 /**
  * 版本
  */
-export interface IVersionVo
-  extends Readonly<{
-    versionNo: string;
-    releaseUrl: string;
-  }> {}
+export interface IVersionVo extends Readonly<{
+  versionNo: string;
+  releaseUrl: string;
+}> {
+} 
