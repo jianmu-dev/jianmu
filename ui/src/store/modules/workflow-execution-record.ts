246a0272d70eef6a1f1edeaaf31c2d386018acbe
liamjung <liamjung@live.com>
2022-01-04 11:34:40 +0800

    refactor: 优化代码

--- ui/src/store/modules/workflow-execution-record.ts
+++ ui/src/store/modules/workflow-execution-record.ts
@@ -87,7 +87,7 @@ export default {
           triggerType: project.triggerType,
         };
       }
-      const taskRecords = !record.id ? [] : await listTask(record.triggerId);
+      const taskRecords = !record.triggerId ? [] : await listTask(record.triggerId);
       commit('mutateRecordDetail', { project, allRecords, record, recordDsl, taskRecords, nodeInfos });
     },
   },
