656ba60ff58ca540cedabbe95b5d5d3875f97958
comyan <1628126452@qq.com>
2022-01-26 15:02:03 +0800

    fix: 查看流程日志中，省略文字未在一行限制[查看流程日志中，省略文字未在一行限制                         https://e.gitee.com/99cloud/issues/list?issue=I4RUND]

---
+++ ui/src/components/text-viewer/text-line.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-html="valueCom" class="text-line"/>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const valueCom = computed<string>(() => props.value.replace(/ /g, '&nbsp;'));
+    return {
+      valueCom,
+    };
+  },
+});
+</script>
+
+<style scoped lang='less'>
+.text-line {
+  &:last-child {
+    text-align: left;
+
+    &::after {
+      width: 0;
+    }
+  }
+
+  text-align: justify;
+  // 兼容IE & 火狐
+  word-break: break-all;
+  text-justify: distribute;
+  // 解决文本内容两端对齐
+  &::after {
+    content: "";
+    display: inline-block;
+    width: 100%;
+  }
+}
+</style>
