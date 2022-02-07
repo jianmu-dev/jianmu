5ecd5e0dc2623ae01a861fb9a03c698ce4d5e096
comyan <1628126452@qq.com>
2022-02-07 10:47:52 +0800

    fix: 分组管理出现横向滚动条 [https://gitee.com/jianmu-dev/jianmu-ci-server/issues/I4RJYY?from=project-issue]

--- ui/src/components/text-viewer/model.ts
+++ ui/src/components/text-viewer/model.ts
@@ -1,4 +1,4 @@
-import { createVNode, nextTick, render, Ref, AppContext, VNode, VNodeProps } from 'vue';
+import { createVNode, nextTick, render, Ref, AppContext, VNode } from 'vue';
 import Tip from './tip.vue';
 import TextLine from './text-line.vue';
 
@@ -56,6 +56,7 @@ export class TextViewer {
       return 0;
     }
     const maxWidth = Math.max(w, this.transitCalculator.value.getBoundingClientRect().width);
+    // 比较到字符内容的最后一位的字符得到最大的宽度值，结束递归。
     if (i === value.length - 1) {
       this.temporaryContent.value = '';
       return maxWidth;
@@ -144,27 +145,31 @@ export class TextViewer {
     i: number = 0,
     contentArr: string[] = [],
   ): Promise<string[]> {
-    this.temporaryContent.value += value[i];
-    await nextTick();
     if (!this.transitCalculator.value) {
       return [];
     }
+    this.temporaryContent.value += value[i];
+    await nextTick();
+    // 获取到此时用于计算的元素中的文字在页面中占据的实际宽度
     let contentWidth = this.transitCalculator.value?.getBoundingClientRect().width;
+    // 提前先判断最后剩余的文本内容占据的宽度是否会大于外层容器的宽度，若大于外层容器的宽度，控制只让最后一行文本是带着...去计算生成截取的文本
     if (rows === contentArr.length + 1 && await this.calculateLastLineMaxWidth(contentArr) > wrapperSize.width) {
       contentWidth += this.ellipsisWidth;
     }
-    // 截取到字符串的offsetWidth大于外层元素设置的宽度终止递归
+    // 截取到字符串的offsetWidth大于外层元素设置的宽度
     if (contentWidth > wrapperSize.width) {
+      // 用于计算的元素中的文本的内容的倒数第二位的文本内容加入数组中，因为此时文本内容已经超过外层盒子的宽度，所以应当减一位。
       contentArr.push(this.temporaryContent.value.substring(0, this.temporaryContent.value.length - 1));
       // 文本内容截取后将响应式占位宽度的span内容清空
       this.temporaryContent.value = '';
-      // 生成多行的VNode中的文本
+      // 如果生成的文本内容数组长度和指定的行数相等递归结束。
       if (rows === contentArr.length) {
         return contentArr;
       }
+      // 当生成的文本内容数组小于指定的行数时，继续重复步骤生成文本字段。
       return await this.generateVNodesInnerText(wrapperSize, value, rows, i, contentArr);
     }
-    // 截取到字符串最后一位递归结束
+    // 截取到字符串最后一位递归结束（当rows计算出得特别大也会在追加到最后一个字符后终止递归）
     if (i === value.length - 1) {
       contentArr.push(this.temporaryContent.value);
       // 文本内容截取后将响应式占位宽度的span内容清空
@@ -182,11 +187,15 @@ export class TextViewer {
    * @return 最后一行文本内容占据的最大宽度
    */
   private async calculateLastLineMaxWidth(contentArr: string[], value: string = this.value.value): Promise<number> {
+    // 将此时用于占据计算元素中的值当成临时变量保存。
     const temp = this.temporaryContent.value;
+    // 获取此时保存在文本数组中的所有文本长度
     const contentLength = contentArr.reduce((pre, current) => {
       return pre + current.length;
     }, 0);
+    // 得到最后一行中的文本内容
     const lastLineValue = value.substring(contentLength);
+    // 获取最后一行文本所占据的宽度值
     const lastLineMaxWidth = await this.calculateContentMaxWidth(lastLineValue);
     this.temporaryContent.value = temp;
     return lastLineMaxWidth;
@@ -216,18 +225,19 @@ export class TextViewer {
       return createVNode('div', { class: 'content' });
     }
     const vNodes: VNode[] = [];
-    // 当前str截取到的索引位置
     for (let i = 0; i < rows; i++) {
-      // 内容生成的数组长度小于指定的rows行数
+      // 如果没有字符内容，进行下次
       if (!contentArr[i]) {
         break;
       }
       const textLineVNode = createVNode(TextLine, { value: contentArr[i], style: { height: `${lineHeight}px` } });
+      // 对非最后一行生成其VNode
       if (i < Math.min(contentArr.length, rows) - 1) {
         // 循环向vNode中追加文本，只有vNode中存在内容时才添加，减少没有意义的vNode
         vNodes.push(textLineVNode);
         continue;
       }
+      // 对contentArr的最后一项的进行单独的判断，如果数组中的最后一位长度刚好是传入进来的值证明最后一行文本没有溢出，不需要生成toolTipVNode
       const contentLength = contentArr.reduce((pre, current) => {
         return pre + current.length;
       }, 0);
@@ -253,14 +263,19 @@ export class TextViewer {
     if (!this.transitCalculator.value) {
       return;
     }
+    // 获取...占据的宽度
     this.ellipsisWidth = await this.calculateEllipsisWidth();
+    // 获取行高
     const lineHeight = await this.calculateLineHeight();
+    // 获取外层元素的宽高，如果外层元素未指定高度，默认将文字占据的行高值设置为高度。
     const wrapperSize = this.calculateContainerSize(lineHeight);
     if (value.length === 0) {
+      // 如果传入的内容为空字符串，生成一个类名为content的空div 作为VNode
       const wrapperNode = await this.generateVNode([], 0, lineHeight, wrapperSize, value);
       render(wrapperNode, this.transitCalculator.value!.parentElement as HTMLElement);
       return;
     }
+    // 获取传入文本中最大的字符占据的宽度。
     const maxCharWidth = await this.calculateMaxCharWidth(value);
     // 如果外层盒子的宽度没有达到一个字符占据的宽度组件不展示内容
     if (wrapperSize.width < maxCharWidth) {
@@ -276,6 +291,7 @@ export class TextViewer {
     const contentArr = await this.generateVNodesInnerText(wrapperSize, value, rows);
     const wrapperNode = await this.generateVNode(contentArr, rows, lineHeight, wrapperSize, value);
     this.temporaryContent.value = '';
+    // 将VNode渲染到页面
     render(wrapperNode, this.transitCalculator.value!.parentElement as HTMLElement);
   }
 
