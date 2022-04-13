7a3293223a53a49c6b9ab8dd7aa6350f58da9b77
liamjung <liamjung@live.com>
2022-04-13 16:55:38 +0800

    refactor: 完善flow node

--- ui/src/components/workflow/workflow-viewer/shapes/flow-node.ts
+++ ui/src/components/workflow/workflow-viewer/shapes/flow-node.ts
@@ -1,17 +1,30 @@
 import _G6 from '@antv/g6';
 import { NodeTypeEnum } from '../utils/enumeration';
+import img from '../svgs/shape/flow-node.svg';
 
 export default function (G6: typeof _G6) {
   G6.registerNode(NodeTypeEnum.FLOW_NODE, {
     options: {
-      size: 25,
-      // anchorPoints: [[0.5, 0], [0.5, 1], [0, 0.5], [1, 0.5]],
+      size: 44,
       anchorPoints: [[0.5, 0.5]],
       style: {
-        // fill: '#C7CFE3',
+        fill: '#FFFFFF',
         lineWidth: 1,
-        stroke: '#C7CFE3',
+        stroke: '#00A6AB',
       },
+    },
+    afterDraw(cfg, group) {
+      const width = 24;
+      const height = 24;
+      group?.addShape('image', {
+        attrs: {
+          x: -width / 2 - 1,
+          y: -height / 2,
+          width,
+          height,
+          img,
+        },
+      });
     },
   }, 'circle');
 }
