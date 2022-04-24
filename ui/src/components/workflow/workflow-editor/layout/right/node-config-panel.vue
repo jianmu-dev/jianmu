6dd2797b6393d7c8c9354b90063df3f7f954c5f7
liamjung <liamjung@live.com>
2022-04-24 16:53:44 +0800

    feat: footer

--- ui/src/components/workflow/workflow-editor/layout/right/node-config-panel.vue
+++ ui/src/components/workflow/workflow-editor/layout/right/node-config-panel.vue
@@ -5,7 +5,13 @@
     direction="rtl"
     destroy-on-close
   >
-    <div class="jm-workflow-editor-node-config-panel">This is node config panel</div>
+    <div class="jm-workflow-editor-node-config-panel">
+      <div>This is node config panel</div>
+      <div class="footer">
+        <jm-button @click="cancel">取消</jm-button>
+        <jm-button type="primary" @click="save">保存</jm-button>
+      </div>
+    </div>
   </jm-drawer>
 </template>
 
@@ -13,14 +19,33 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  setup(props, context) {
-
+  emits: ['update:model-value'],
+  setup(props, { emit }) {
+    return {
+      cancel: () => {
+        emit('update:model-value', false);
+      },
+      save: () => {
+        emit('update:model-value', false);
+      },
+    };
   },
 });
 </script>
 
 <style scoped lang="less">
 .jm-workflow-editor-node-config-panel {
-
+  .footer {
+    box-sizing: border-box;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    display: flex;
+    border-top: 1px solid #6b7b8d;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
