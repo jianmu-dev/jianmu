3462b630b6b7fa5796ca97a988300d7223252f52
liamjung <liamjung@live.com>
2022-05-04 16:55:28 +0800

    feat: 关闭洁面配置面板时，取消选中节点

--- ui/src/components/workflow/workflow-editor/layout/right/node-config-panel.vue
+++ ui/src/components/workflow/workflow-editor/layout/right/node-config-panel.vue
@@ -55,7 +55,8 @@ export default defineComponent({
   },
   setup(props, { emit }) {
     const getGraph = inject('getGraph') as () => Graph;
-    const node = getGraph().getNodes().find(({ id }) => props.nodeId === id)!;
+    const graph = getGraph();
+    const node = graph.getNodes().find(({ id }) => props.nodeId === id)!;
     const proxy = new CustomX6NodeProxy(node);
     // 不能为ref，否则，表单内容的变化影响数据绑定
     const nodeData = proxy.getData();
@@ -77,6 +78,9 @@ export default defineComponent({
       cancel: () => {
         // 关闭抽屉
         emit('update:model-value', false);
+
+        // 取消选中
+        graph.unselect(node);
       },
       save: () => {
         formRef.value.validate((valid: boolean) => {
@@ -86,6 +90,9 @@ export default defineComponent({
           proxy.setData(nodeData);
           // 关闭抽屉
           emit('update:model-value', false);
+
+          // 取消选中
+          graph.unselect(node);
 
           // 通过校验时，删除警告
           node.removeTool('button');
