6489ceaab86f08be3bab3e5105ee8c73d806064d
liamjung <liamjung@live.com>
2022-05-06 11:28:56 +0800

    refactor: 优化定义

--- ui/src/components/workflow/workflow-editor/model/data/node/shell.ts
+++ ui/src/components/workflow/workflow-editor/model/data/node/shell.ts
@@ -10,18 +10,18 @@ export interface IShellEnv {
 export class Shell extends BaseNode {
   image: string;
   readonly envs: IShellEnv[];
-  readonly scripts: string[];
+  script: string;
 
   constructor(name: string = 'shell', image: string = '',
-    envs: IShellEnv[] = [], scripts: string[] = []) {
+    envs: IShellEnv[] = [], script: string = '') {
     super('shell', name, NodeTypeEnum.SHELL, icon, 'https://docs.jianmu.dev/guide/shell-node.html');
     this.image = image;
     this.envs = envs;
-    this.scripts = scripts;
+    this.script = script;
   }
 
-  static build({ name, image, envs, scripts }: any): Shell {
-    return new Shell(name, image, envs, scripts);
+  static build({ name, image, envs, script }: any): Shell {
+    return new Shell(name, image, envs, script);
   }
 
   getFormRules(): any {
@@ -30,6 +30,10 @@ export class Shell extends BaseNode {
     return {
       ...rules,
       // TODO 待完善校验规则
+      image: [],
+      env_name: [],
+      env_value: [],
+      script: [],
     };
   }
 }
