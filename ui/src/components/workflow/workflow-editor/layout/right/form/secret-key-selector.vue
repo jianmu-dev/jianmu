8c26898dc48067089c3c023fea8085fda375a035
qbhfh <2682147541@qq.com>
2022-05-10 15:27:02 +0800

    feat: 新增选择密钥和webhook参数组件

---
+++ ui/src/components/workflow/workflow-editor/layout/right/form/secret-key-selector.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="secret-key-selector">
+    <jm-cascader :props="cascaderProps" clearable placeholder="请选择密钥" v-model="selectorValue"
+                 @change="getValue"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import { listNamespace, listSecretKey } from '@/api/view-no-auth';
+
+export interface ISecretKey {
+  namespace: string;
+  key: string;
+}
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: Object as PropType<ISecretKey>,
+    },
+  },
+  emits: ['update:model-value'],
+  setup(props, { emit }) {
+
+    const selectorValue = ref<string[]>(props.modelValue ? [props.modelValue.namespace, props.modelValue.key] : []);
+    return {
+      selectorValue,
+      getValue: (val: string[] | undefined) => {
+        emit('update:model-value', val ? { namespace: val[0], key: val[1] } : undefined);
+      },
+      cascaderProps: {
+        lazy: true,
+        lazyLoad: async (node: any, resolve: any) => {
+          const { level } = node;
+          const nodes: object[] = [];
+
+          if (level === 0) {
+            const { list } = await listNamespace();
+            list.forEach(({ name }) => nodes.push({
+              value: name,
+              label: name,
+              leaf: false,
+            }));
+          } else {
+            const { value } = node;
+            const skArr = await listSecretKey(value);
+            skArr.forEach(sk => nodes.push({
+              value: sk,
+              label: sk,
+              leaf: true,
+            }));
+          }
+
+          // 通过调用resolve将子节点数据返回，通知组件数据加载完成
+          resolve(nodes);
+        },
+      },
+    };
+  },
+});
+</script>
