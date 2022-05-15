55e699fbffee30632c6606b76a97a711ced85f1b
liamjung <liamjung@live.com>
2022-05-15 23:44:52 +0800

    refactor: 优化代码

--- ui/src/components/workflow/workflow-viewer/utils/graph.ts
+++ ui/src/components/workflow/workflow-viewer/utils/graph.ts
@@ -7,6 +7,7 @@ import { size } from '@/components/workflow/workflow-viewer/shapes/async-task';
 import { INodeMouseoverEvent } from '@/components/workflow/workflow-viewer/utils/model';
 import { INodeDefVo } from '@/api/dto/project';
 import './array';
+import { sortTasks } from '../model/util';
 
 /**
  * 检查内容是否溢出
@@ -258,31 +259,6 @@ export function init(dsl: string | undefined, triggerType: TriggerTypeEnum | und
   });
 
   return graph;
-}
-
-/**
- * 排序任务列表
- * @param tasks
- * @param desc
- * @param nodeName
- */
-export function sortTasks(tasks: ITaskExecutionRecordVo[], desc: boolean, nodeName?: string): ITaskExecutionRecordVo[] {
-  if (nodeName) {
-    tasks = tasks.filter(task => task.nodeName === nodeName);
-  }
-
-  // 按开始时间降序排序
-  return tasks.sort((t1, t2) => {
-    const st1 = Date.parse(t1.startTime);
-    const st2 = Date.parse(t2.startTime);
-    if (st1 === st2) {
-      return 0;
-    }
-    if (st1 > st2) {
-      return desc ? -1 : 1;
-    }
-    return desc ? 1 : -1;
-  });
 }
 
 /**
