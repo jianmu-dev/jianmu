005150de4b732fe24a6edd39bb06ab67f61063c7
daihw <928874202@qq.com>
2022-07-13 11:04:50 +0800

    feat: V3.0.0__Init_Tables.sql

--- api/src/main/resources/db/migration/V2.3.0__Update_Workflow_Table.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `workflow`
-    add `created_time` datetime DEFAULT NULL COMMENT '创建时间';
-
-ALTER TABLE `workflow_instance`
-    add `suspended_time` datetime DEFAULT NULL COMMENT '挂起时间' AFTER `start_time`;
-
-ALTER TABLE `async_task_instance`
-    add `failure_mode` varchar(45) DEFAULT 'SUSPEND' COMMENT '错误处理模式' AFTER `status`;
