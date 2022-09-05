7b5a11a53f57546edebe39c231e495f3833ec058
daihw <928874202@qq.com>
2022-09-05 15:11:46 +0800

    refactor: IgnoreSyncDslEventException.java

--- application/src/main/java/dev/jianmu/application/exception/IgnoreSyncDslEventException.java
+++ application/src/main/java/dev/jianmu/application/exception/IgnoreSyncDslEventException.java
@@ -2,7 +2,7 @@ package dev.jianmu.application.exception;
 
 /**
  * @class IgnoreSyncEventException
- * @description 忽略同步流水线触发的webhook
+ * @description 忽略同步流水线DSL事件异常
  * @author Daihw
  * @create 2022/9/5 2:39 下午
  */
