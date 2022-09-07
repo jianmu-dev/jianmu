75e432771876aeeed2624a5d10e1bd34b154dfe8
wangjiaguo <351786404@qq.com>
2022-09-07 18:43:39 +0800

    fix: 修复raw-data下标

--- ui/src/components/workflow/workflow-viewer/model/workflow-graph.ts
+++ ui/src/components/workflow/workflow-viewer/model/workflow-graph.ts
@@ -25,9 +25,9 @@ export class WorkflowGraph {
     this.triggerType = triggerType;
     this.container = container;
     this.configNodeCallbackFn = configNodeCallbackFn;
-    if (dsl.indexOf('raw-data') !== -1) {
+    if (dsl.lastIndexOf('raw-data: "{') !== -1) {
       this.isX6 = true;
-      this.visibleDsl = dsl.substring(0, dsl.indexOf('raw-data')).trim();
+      this.visibleDsl = dsl.substring(0, dsl.lastIndexOf('raw-data: "{')).trim();
     } else {
       this.isX6 = false;
       this.visibleDsl = dsl;
