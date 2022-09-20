f6d9ecb5d7a7b3c8830ea5c342774e73790f0a03
daihw <928874202@qq.com>
2022-09-20 15:24:09 +0800

    refactor: 修改流程/任务SSE连接timeout为60s

---
+++ event/src/main/java/dev/jianmu/event/impl/SseConnectedEvent.java
@@ -0,0 +1,10 @@
+package dev.jianmu.event.impl;
+
+/**
+ * @class SseConnectedEvent
+ * @description SSE连接成功事件
+ * @author Daihw
+ * @create 2022/9/20 3:23 下午
+ */
+public class SseConnectedEvent extends BaseEvent {
+}
