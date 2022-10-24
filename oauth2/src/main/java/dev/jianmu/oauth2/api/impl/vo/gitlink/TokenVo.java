51fed95dc4f3ad786e9809a96d89926ac18690d1
hx <1014231151@qq.com>
2022-10-24 14:00:42 +0800

    feat: 调用第三方API返回字段为空时，自动忽略

--- oauth2/src/main/java/dev/jianmu/oauth2/api/impl/vo/gitlink/TokenVo.java
+++ oauth2/src/main/java/dev/jianmu/oauth2/api/impl/vo/gitlink/TokenVo.java
@@ -1,5 +1,6 @@
 package dev.jianmu.oauth2.api.impl.vo.gitlink;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.jianmu.oauth2.api.config.OAuth2Properties;
 import dev.jianmu.oauth2.api.exception.UnknownException;
@@ -17,6 +18,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TokenVo implements ITokenVo {
     @JsonProperty("access_token")
     private String accessToken;
