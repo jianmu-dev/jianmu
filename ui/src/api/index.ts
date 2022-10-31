541f9e5b69f70f2495755edd0e5b38b573591d79
comyan <1628126452@qq.com>
2022-10-31 17:57:25 +0800

    feat: 增加API出现ConflictError错误时刷新session机制

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,5 +1,20 @@
 import rest, { IRequest } from '@/utils/rest';
+import { ConflictError } from '@/utils/rest/error';
+import { ISession } from '@/model/modules/session';
+import { namespace } from '@/store/modules/session';
+import _store from '@/store';
+import { setAuthCookie } from '@/utils/cookie';
 
 export async function restProxy<T = any>(request: IRequest): Promise<T> {
-  return await rest(request);
+  try {
+    return await rest(request);
+  } catch (err) {
+    if (err instanceof ConflictError) {
+      const session = err.response.data as ISession;
+      setAuthCookie(session);
+      _store.commit(`${namespace}/mutateSession`);
+      return await rest(request);
+    }
+    throw err;
+  }
 }
