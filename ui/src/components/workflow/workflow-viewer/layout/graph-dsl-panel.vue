e11e0724bb81810a1f1b8fde17f7ed1d991a93a6
wllgogogo <351786404@qq.com>
2022-11-15 17:29:11 +0800

    fix: 删除console调试信息

--- ui/src/components/workflow/workflow-viewer/layout/graph-dsl-panel.vue
+++ ui/src/components/workflow/workflow-viewer/layout/graph-dsl-panel.vue
@@ -104,7 +104,6 @@ export default defineComponent({
       getGraphData();
     };
     onUpdated(() => {
-      console.log('11');
       // 执行动画
       executeAnimation();
       // 切换record 重新生成画布
@@ -112,7 +111,6 @@ export default defineComponent({
     });
     // 保证整个视图都渲染完毕，才能确定图的宽高
     onMounted(() => {
-      console.log('22');
       // g6刷新详情不会正常渲染画布问题 预览需要执行画布
       if (props.readonly) {
         getGraphData();
