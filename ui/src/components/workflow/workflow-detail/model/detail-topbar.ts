ae94e79f8b75fbdde8c79963d53da8664167b97c
wllgogogo <351786404@qq.com>
2022-12-29 14:49:35 +0800

    feat: 添加终止全部按钮

--- ui/src/components/workflow/workflow-detail/model/detail-topbar.ts
+++ ui/src/components/workflow/workflow-detail/model/detail-topbar.ts
@@ -1,18 +1,37 @@
 import { executeImmediately } from '@/api/project';
+import { terminateAll } from '@/api/workflow-execution-record';
 
-type TriggerCallbackFnType = (error?: Error)=>void;
+type TriggerCallbackFnType = (error?: Error) => void;
 
 export class DeatilTopbar {
   private readonly projectId: string;
+  private readonly workflowRef: string;
   private readonly triggerCallbackFn: TriggerCallbackFnType;
-  constructor(projectId: string, triggerCallbackFn: TriggerCallbackFnType) {
+  constructor(projectId: string, workflowRef: string, triggerCallbackFn: TriggerCallbackFnType) {
     this.projectId = projectId;
+    this.workflowRef = workflowRef;
     this.triggerCallbackFn = triggerCallbackFn;
+  }
+  /**
+   * 终止所有流程
+   */
+  async terminateAllRecord(): Promise<void> {
+    try {
+      await terminateAll(this.workflowRef);
+      this.triggerCallbackFn();
+    } catch (error: any) {
+      if (error.response.status === 400) {
+        console.log('触发新流程 error', error.response.data.message);
+        this.triggerCallbackFn(error);
+      } else {
+        this.triggerCallbackFn(error);
+      }
+    }
   }
   /**
    * 触发新流程
    */
-  async trigger():Promise<void> {
+  async trigger(): Promise<void> {
     try {
       await executeImmediately(this.projectId);
       this.triggerCallbackFn();
