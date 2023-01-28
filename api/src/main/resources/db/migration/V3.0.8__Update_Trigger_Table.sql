fce558726f417e75b6fb335e73e5c93536fdb3d9
daihw <928874202@qq.com>
2023-01-28 17:59:55 +0800

    refactor: V3.0.8__Update_Trigger_Table.sql

--- api/src/main/resources/db/migration/V3.0.8__Update_Trigger_Table.sql
+++ api/src/main/resources/db/migration/V3.0.8__Update_Trigger_Table.sql
@@ -1,6 +1,5 @@
 ALTER TABLE `jm_trigger`
     MODIFY `ref` varchar(512) NULL COMMENT '唯一标识';
 
-UPDATE `jm_trigger` SET `ref` = null where `type` = 'CRON';
-UPDATE `jm_trigger` SET `webhook` = null where `type` = 'CRON';
+UPDATE `jm_trigger` SET `ref` = null, `webhook` = null where `type` = 'CRON';
 UPDATE `jm_trigger` SET `schedule` = null where `type` = 'WEBHOOK';
