1a976b88625ce7ecdad0e0948e9092f0f687d400
wllgogogo <351786404@qq.com>
2023-03-13 10:16:44 +0800

    fix: 实例流程节点操作图标统一

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,24 +2,21 @@ import { ConfigEnv, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { name, version } from './package.json';
-const target = 'http://localhost:8081';
+const target = 'https://ci.test.jianmuhub.com';
 const changeOrigin = true;
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfigExport => {
   return {
     plugins: [vue()],
     // base public path
-    base:
-      command === 'build' && mode === 'cdn'
-        ? `https://jianmu-ci.assets.dghub.cn/${name}/v${version}/`
-        : '/',
+    base: command === 'build' && mode === 'cdn' ? `https://jianmu-ci.assets.dghub.cn/${name}/v${version}/` : '/',
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src'),
         // 解决：
         // Component provided template option but runtime compilation is not supported in this build of Vue
         // Configure your bundler to alias "vue" to "vue/dist/vue.esm-bundler.js".
-        'vue': 'vue/dist/vue.esm-bundler.js',
+        vue: 'vue/dist/vue.esm-bundler.js',
       },
     },
     server: {
