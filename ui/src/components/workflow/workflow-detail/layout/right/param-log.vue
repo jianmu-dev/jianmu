e5c86f517d49bf8a8ca6110a0ec85561dd4ca09c
wllgogogo <351786404@qq.com>
2023-04-23 18:08:25 +0800

    fix: 全局参数脱敏展示切换

--- ui/src/components/workflow/workflow-detail/layout/right/param-log.vue
+++ ui/src/components/workflow/workflow-detail/layout/right/param-log.vue
@@ -4,70 +4,34 @@
       <jm-table
         :data="globalParams"
         :border="true"
-        :cell-class-name="
-          ({ row, columnIndex }) =>
-            row.required && columnIndex === 0 ? 'required-cell' : ''
-        "
+        :cell-class-name="({ row, columnIndex }) => (row.required && columnIndex === 0 ? 'required-cell' : '')"
       >
-        <jm-table-column
-          label="参数唯一标识"
-          header-align="center"
-          width="200px"
-          prop="ref"
-        >
+        <jm-table-column label="参数唯一标识" header-align="center" width="200px" prop="ref">
           <template #default="scope">
             <div
               :style="{
-                maxWidth: maxWidthRecord[scope.row.ref]
-                  ? `${maxWidthRecord[scope.row.ref]}px`
-                  : '100%',
+                maxWidth: maxWidthRecord[scope.row.ref] ? `${maxWidthRecord[scope.row.ref]}px` : '100%',
               }"
             >
               <div class="text-viewer">
-                <jm-text-viewer :threshold="0"
+                <jm-text-viewer
+                  :threshold="0"
                   :value="scope.row.ref"
                   class="value"
                   :tip-append-to-body="false"
-                  @loaded="
-                    ({ contentMaxWidth }) =>
-                      getTotalWidth(contentMaxWidth, scope.row.ref)
-                  "
+                  @loaded="({ contentMaxWidth }) => getTotalWidth(contentMaxWidth, scope.row.ref)"
                 />
               </div>
-              <jm-tooltip
-                content="必填项"
-                placement="top"
-                :appendToBody="false"
-                v-if="scope.row.required"
-              >
+              <jm-tooltip content="必填项" placement="top" :appendToBody="false" v-if="scope.row.required">
                 <img src="~@/assets/svgs/task-log/required.svg" alt="" />
               </jm-tooltip>
             </div>
           </template>
         </jm-table-column>
-        <jm-table-column
-          header-align="center"
-          label="名称"
-          width="120px"
-          align="center"
-          prop="name"
-        >
+        <jm-table-column header-align="center" label="名称" width="120px" align="center" prop="name"> </jm-table-column>
+        <jm-table-column header-align="center" label="参数类型" width="100px" align="center" prop="type">
         </jm-table-column>
-        <jm-table-column
-          header-align="center"
-          label="参数类型"
-          width="100px"
-          align="center"
-          prop="type"
-        >
-        </jm-table-column>
-        <jm-table-column
-          label="是/否必填"
-          header-align="center"
-          align="center"
-          width="100px"
-          prop="required"
-        >
+        <jm-table-column label="是/否必填" header-align="center" align="center" width="100px" prop="required">
           <template #default="scope">
             <span :class="[scope.row.required ? 'is-required' : '']">
               {{ scope.row.required ? '是' : '否' }}
@@ -76,11 +40,38 @@
         </jm-table-column>
         <jm-table-column label="参数值" header-align="center">
           <template #default="scope">
-            <param-value
-              :value="scope.row.value"
-              :tip-append-to-body="false"
-              :type="scope.row.hidden? ParamTypeEnum.SECRET:scope.row.type"
-            />
+            <div v-if="scope.row.type === ParamTypeEnum.SECRET">
+              <!-- 密钥类型切换 -->
+              <div class="hide-container" v-if="secretVisible">
+                <span>******</span>
+                <i class="hide-secret jm-icon-input-invisible" @click="hideSecret"></i>
+              </div>
+              <div class="display-container" v-else>
+                <template v-if="scope.row.value">
+                  <div
+                    class="param-value"
+                    :style="{
+                      maxWidth: maxWidthRecord[scope.row.value] ? `${maxWidthRecord[scope.row.value]}px` : '100%',
+                    }"
+                  >
+                    <jm-text-viewer
+                      v-if="scope.row.valueType !== ParamTypeEnum.SECRET"
+                      :value="scope.row.value"
+                      @loaded="({ contentMaxWidth }) => getTotalWidth(contentMaxWidth, scope.row.value)"
+                      class="value"
+                    >
+                    </jm-text-viewer>
+                    <template v-else>
+                      {{ scope.row.value }}
+                    </template>
+                  </div>
+                </template>
+                <i class="display-secret jm-icon-input-visible" @click="displaySecret"></i>
+              </div>
+            </div>
+            <template v-else>
+              <param-value :value="scope.row.value" :type="scope.row.valueType" />
+            </template>
           </template>
         </jm-table-column>
       </jm-table>
@@ -91,21 +82,27 @@
 <script lang="ts">
 import { defineComponent, PropType, ref } from 'vue';
 import ParamValue from '@/views/common/param-value.vue';
-import { IGlobalParamseterVo } from '@/api/dto/project';
+import { IGlobalParameterVo } from '@/api/dto/project';
 import { ParamTypeEnum } from '@/api/dto/enumeration';
 
 export default defineComponent({
   components: { ParamValue },
   props: {
     globalParams: {
-      type: Array as PropType<IGlobalParamseterVo[]>,
+      type: Array as PropType<IGlobalParameterVo[]>,
       required: true,
     },
   },
   setup() {
+    // 密钥类型显隐
+    const secretVisible = ref<boolean>(true);
     const maxWidthRecord = ref<Record<string, number>>({});
     return {
       ParamTypeEnum,
+      // 密钥类型显隐
+      secretVisible,
+      hideSecret: () => (secretVisible.value = false),
+      displaySecret: () => (secretVisible.value = true),
       maxWidthRecord,
       getTotalWidth(width: number, ref: string) {
         maxWidthRecord.value[ref] = width;
@@ -159,7 +156,7 @@ export default defineComponent({
               position: absolute;
               left: 100%;
               margin-left: 5px;
-              bottom: 0
+              bottom: 0;
             }
           }
         }
@@ -177,6 +174,67 @@ export default defineComponent({
         .is-required {
           color: #ff0000;
         }
+      }
+    }
+  }
+
+  .hide-secret,
+  .display-secret {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    border-radius: 4px;
+    cursor: pointer;
+    //margin-left: 10px;
+  }
+
+  .hide-container {
+    display: flex;
+    align-items: center;
+    line-height: 20px;
+
+    span {
+      margin-right: 5px;
+    }
+
+    .hide-secret {
+      position: relative;
+      top: -2px;
+
+      .jm-icon-input-visible::before {
+        content: '\e803';
+      }
+
+      &:hover {
+        background: #eff7ff;
+      }
+    }
+  }
+
+  .display-container {
+    display: flex;
+    align-items: center;
+    line-height: 20px;
+    //justify-content: space-between;
+    .param-value {
+      flex: 1;
+      margin-right: 5px;
+    }
+
+    .trigger-params-value {
+      width: 280px;
+    }
+
+    .display-secret {
+      .jm-icon-input-invisible::before {
+        content: '\e800';
+      }
+
+      &:hover {
+        background: #eff7ff;
       }
     }
   }
