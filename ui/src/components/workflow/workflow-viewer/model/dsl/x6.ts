2f0cc78b117198a8657a6f250fa8e347b8498efd
liamjung <liamjung@live.com>
2023-05-12 13:00:28 +0800

    fix: 触发器设置为cron，手动触发后，详情中回显的触发器有误[https://gitee.com/jianmu-dev/jianmu/issues/I6ZTNG]

--- ui/src/components/workflow/workflow-viewer/model/dsl/x6.ts
+++ ui/src/components/workflow/workflow-viewer/model/dsl/x6.ts
@@ -1,5 +1,7 @@
+import { Cell } from '@antv/x6';
 import yaml from 'yaml';
 import { DslTypeEnum, TriggerTypeEnum } from '@/api/dto/enumeration';
+import { NodeTypeEnum } from '../data/enumeration';
 
 export function parse(
   dsl: string | undefined,
@@ -13,10 +15,39 @@ export function parse(
     return { dslType: DslTypeEnum.PIPELINE, asyncTaskRefs: [], data: '' };
   }
 
-  const { workflow, pipeline, 'raw-data': rawData } = yaml.parse(dsl);
+  const { trigger, workflow, pipeline, 'raw-data': rawData } = yaml.parse(dsl);
+
+  let data: string;
+  if (trigger && trigger.type === NodeTypeEnum.CRON && triggerType === TriggerTypeEnum.MANUAL) {
+    let triggerNodeId: string | undefined;
+    const cells = (JSON.parse(rawData).cells as Cell.Properties[])
+      // 过滤cron触发器
+      .filter(cell => {
+        if (cell.shape !== 'vue-shape') {
+          return true;
+        }
+        const { type } = JSON.parse(cell.data);
+        if (NodeTypeEnum.CRON === type) {
+          triggerNodeId = cell.id;
+          return false;
+        }
+        return true;
+      })
+      // 过滤触发器相关边
+      .filter(cell => {
+        if (cell.shape !== 'edge') {
+          return true;
+        }
+        return triggerNodeId !== cell.source.cell;
+      });
+    data = JSON.stringify({ cells });
+  } else {
+    data = rawData;
+  }
+
   return {
     dslType: workflow ? DslTypeEnum.WORKFLOW : DslTypeEnum.PIPELINE,
     asyncTaskRefs: Object.keys(workflow || pipeline),
-    data: rawData,
+    data,
   };
 }
