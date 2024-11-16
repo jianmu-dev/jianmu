dc3a4e517f375f47f2c12ab9f0745f8a0d13a2d1
ethan <ethan-liu@outlook.com>
2024-11-16 18:42:25 +0800

    fix: WorkerVo未加@Data注解导致无法返回值

--- api/src/main/java/dev/jianmu/api/vo/WorkerVo.java
+++ api/src/main/java/dev/jianmu/api/vo/WorkerVo.java
@@ -4,11 +4,13 @@ import dev.jianmu.worker.aggregate.Worker;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "Worker定义VO")
